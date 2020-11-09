32e60215f467d2b95608779db9179c63b494b3bd
AmFadma <fadma.amiche@u-psud.fr>
2020-11-09 17:51:09 +0100

    modification frog.java class frog and frog(Game game);

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,28 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+
+	private Direction dir;
+	private Case pos;
+
+	public Frog(Game game) {
+		this.game = game;
+		this.dir = Direction.up;
+		this.pos = new Case(game.width / 2, 0);
+	}
+	public Case getPosition (){
+		return this.pos;
+
+
+	}
+
+
 
 
 }
